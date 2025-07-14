4fe156d4873eaadcd63692c8cb542d5d75a5a3d5
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-07-14 10:03:58 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 //DESAFIO 01
 
-//let numeroInicial = 1;
-//let numeroAlvo = 10;
+let numeroInicial = 1;
+let numeroAlvo = 10;
 
-//while (numeroInicial <= 11) {
-//    let contador = numeroInicial++;
-//    alert(contador);
-//}
+while (numeroInicial <= 11) {
+    let contador = numeroInicial++;
+    alert(contador);
+}
 
 
 
 //DESAFIO 02
 
-//let numeroInicial= 10;
-//let numeroAlvo = 0;
+let numeroInicial= 10;
+let numeroAlvo = 0;
 
-//while(numeroInicial >= 0) {
-//    let contador = numeroInicial--;
-//    alert(contador);
-//}
+while(numeroInicial >= 0) {
+    let contador = numeroInicial--;
+    alert(contador);
+}
 
 //DESAFIO 03
 
-//let numeroAlvo = 0;
-//let numeroInicial = prompt("Diga um número aleatório");
+let numeroAlvo = 0;
+let numeroInicial = prompt("Diga um número aleatório");
 
-//while (numeroInicial >= numeroAlvo) {
-//    let contador = numeroInicial--;
-//    console.log(contador);
-//    alert(contador);
-//}
+while (numeroInicial >= numeroAlvo) {
+    let contador = numeroInicial--;
+    console.log(contador);
+    alert(contador);
+}
 
 //DESAFIO 04
 
